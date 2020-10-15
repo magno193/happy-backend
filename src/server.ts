3335aea59e58c0f2d8851cb36fca38e4b2d1f6b9
Alexandre Magno de Lima Ferreira <alexmlf93@gmail.com>
2020-10-15 00:14:34 -0300

    Refatorando código para rotas e controller

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,11 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import './database/connection';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
+import routes from './routes';
 
 const app = express();
 app.use(cors());
 app.use(json());
-
-app.post<Orphanage>('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  }: Orphanage = request.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-  await orphanagesRepository.save(orphanage);
-
-  return response.status(201).json(orphanage);
-});
+app.use(routes);
 
 app.listen(3333);
